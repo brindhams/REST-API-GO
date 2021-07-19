6b5cef6d0d05551e8c0d6c10220cdbffc0012a88
brindhams <85442521+brindhams@users.noreply.github.com>
2021-07-19 15:04:14 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"handlers"
+
+	"github.com/gorilla/mux"
+)
+	
+type home struct {
+	Message string `json:"message"`
+}
+
+func homelink(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(home{Message: "Welcome"})
+	
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homelink).Methods("GET")
+	router.HandleFunc("/products", handlers.Getallproducts).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+	
+
+}
+
+
+
+
