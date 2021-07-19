4809fea6e13b5e4fdd6cdefce008f381f0394f0a
brindhams <85442521+brindhams@users.noreply.github.com>
2021-07-19 15:05:56 +0530

    Add files via upload

---
+++ Database/Databaseconnection.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"log"
+    "fmt"
+
+	"github.com/gocql/gocql")
+
+
+type DBConnection struct {
+    cluster *gocql.ClusterConfig
+    session *gocql.Session
+}
+var connection DBConnection
+
+func SetupDBConnection() {
+    var err error
+
+    connection.cluster = gocql.NewCluster("127.0.0.1")
+    connection.cluster.ProtoVersion = 3
+    connection.cluster.Keyspace = "ecommerce"
+    connection.session, err =connection.cluster.CreateSession()
+    if err !=nil {
+        log.Fatal(err)
+    }
+    fmt.Println("cassandra initialized")
+    
+    
+}
+
